64846e4bda0625c4c881f5af2c3517568238746f
xanedesu <xanedesu@gmail.com>
2022-04-16 23:28:54 +0300

    Обновил структуру

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleType } from '@prisma/client';
-import { ROLES_KEY } from './roles.decorator';
+import { ROLES_KEY } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
